fe254528df59750fc219e0ee943ce6812234b787
lnara002 <lakshman_kumar.narayanan@alcatel-lucent.com>
2015-12-17 09:54:35 -0800

    adding a guess-value

--- mastermind.py
+++ mastermind.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import random
+
 class Colors:
   color_list = [
     ("red",    'R'),  # 0
@@ -10,9 +12,17 @@ class Colors:
     ("brown",  'B'),  # 5
     ("Last-Non-color-Count", "?") ]
 
+def make_a_guess(guess_size):
+  guess=[]
+  for i in range(guess_size):
+    guess.append(random.randint(0,guess_size-1))
+  return guess
+
 def main():
-    print Colors.color_list
+  print Colors.color_list
+  g = make_a_guess(6)
+  print g
 
 if __name__ == '__main__':
-    main()
+  main()
 
